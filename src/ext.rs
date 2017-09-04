8ecb2bd251d5b0c5a737efbdf6e9a2aa0fa7ae50
NikVolf <nikvolf@gmail.com>
2017-09-04 14:12:07 +0200

    add mapping to externs

--- src/ext.rs
+++ src/ext.rs
@@ -47,6 +47,19 @@ mod external {
         ) -> i32;
 
         // enviromental blockchain functions (runtume might not provide all of these!)
+
+        /// Block hash of the specific block
+        pub fn blockhash(number: i64, dest: *mut u8) -> i32;
+
+        pub fn coinbase(dest: *mut u8);
+
+        pub fn timestamp() -> i64;
+
+        pub fn blocknumber() -> i64;
+
+        pub fn difficulty(dest: *mut u8);
+
+        pub fn gaslimit(dest: *mut u8);
     }
 }
 
@@ -93,19 +106,37 @@ pub fn static_call(address: &[u8; 20], input: &[u8], result: &mut [u8]) -> Resul
 }
 
 pub fn block_hash(block_number: u64) -> Result<[u8; 32], Error> {
+    let mut res = [0u8; 32];
+    unsafe {
+        match external::blockhash(block_number as i64, res.as_mut_ptr()) {
+            0 => Ok(res),
+            _ => Err(Error),
+        }
+    }
 }
 
 pub fn coinbase() -> [u8; 20] {
+    let mut res = [0u8; 20];
+    unsafe { external::coinbase(res.as_mut_ptr()); }
+    res
 }
 
 pub fn timestamp() -> u64 {
+    unsafe { external::timestamp() as u64 }
 }
 
 pub fn block_number() -> u64 {
+    unsafe { external::blocknumber()  as u64 }
 }
 
 pub fn difficulty() -> [u8; 32] {
+    let mut res = [0u8; 32];
+    unsafe { external::difficulty(res.as_mut_ptr()); }
+    res
 }
 
 pub fn gas_limit() -> [u8; 32] {
+    let mut res = [0u8; 32];
+    unsafe { external::gaslimit(res.as_mut_ptr()); }
+    res
 }
