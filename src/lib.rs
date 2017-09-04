fb322b9a2761c083bb47ad42502a7a2fbdef8fc3
NikVolf <nikvolf@gmail.com>
2017-09-04 12:50:08 +0200

    reorg alloc features

--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,19 @@
 #![feature(lang_items)]
 #![feature(link_args)]
 #![feature(alloc)]
-#![feature(collections)]
 #![feature(macro_reexport)]
 
 extern crate wasm_alloc;
-extern crate alloc;
-#[macro_reexport(vec)] extern crate collections;
+#[macro_reexport(vec)] extern crate alloc;
 
 pub extern crate bigint;
 
 use core::{slice, ptr, mem};
 use core::ops::Shl;
 pub use alloc::boxed::Box;
-pub use collections::Vec;
+pub use alloc::string::String;
+pub use alloc::str;
+pub use alloc::vec::Vec;
 pub use wasm_alloc::WamsAllocator;
 
 /// Wrapper over storage read/write externs
