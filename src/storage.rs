a03e2914081538c432e5ff39acac2f0332f84e9f
Sergey Pepyakin <s.pepyakin@gmail.com>
2017-11-20 16:27:58 +0300

    Remove storage error.

--- src/storage.rs
+++ src/storage.rs
@@ -8,28 +8,23 @@ pub struct Error;
 
 #[cfg_attr(not(feature="std"), link(name = "env"))]
 extern {
-    fn storage_read(key: *const u8, dst: *mut u8) -> i32;
-    fn storage_write(key: *const u8, src: *const u8) -> i32;
+    fn storage_read(key: *const u8, dst: *mut u8);
+    fn storage_write(key: *const u8, src: *const u8);
 }
 
 /// Performs read from storage
 /// Can return `Error` if data is read from outside of the storage boundaries
-pub fn read(key: &H256) -> Result<[u8; 32], Error> {
+pub fn read(key: &H256) -> [u8; 32] {
     let mut dst = [0u8; 32];
-    match unsafe {
-        storage_read(key.as_ptr(), (&mut dst).as_mut_ptr())
-    } {
-        x if x < 0 => Err(Error),
-        _ => Ok(dst),
+    unsafe {
+        storage_read(key.as_ptr(), dst.as_mut_ptr());
     }
+	dst
 }
 
 /// Performs write to the storage
-pub fn write(key: &H256, val: &[u8; 32]) -> Result<(), Error> {
-    match unsafe {
-        storage_write(key.as_ptr(), val.as_ptr())
-    } {
-        x if x < 0 => Err(Error),
-        _ => Ok(()),
+pub fn write(key: &H256, val: &[u8; 32]) {
+    unsafe {
+        storage_write(key.as_ptr(), val.as_ptr());
     }
 }
