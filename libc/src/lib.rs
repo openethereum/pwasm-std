ee1755a5a63906029b8d39a8861f9b9ba7f0a26d
Sergey Pepyakin <s.pepyakin@gmail.com>
2018-02-02 20:00:29 +0300

    Use rlibc

--- libc/src/lib.rs
+++ libc/src/lib.rs
@@ -2,38 +2,6 @@
 #![cfg_attr(feature = "strict", deny(warnings))]
 #![no_std]
 
-//! libc externs crate
+//! libc crate
 
-extern "C" {
-	fn ext_memcmp(cx: *const u8, ct: *const u8, n: usize) -> i32;
-	fn ext_memcpy(dest: *mut u8, src: *const u8, n: usize) -> *mut u8;
-	fn ext_memmove(dest: *mut u8, src: *const u8, n: usize) -> *mut u8;
-	fn ext_memset(dest: *mut u8, c: i32, n: usize) -> *mut u8;
-}
-
-// Declaring these function here prevents Emscripten from including it's own verisons
-// into final binary.
-
-/// memcmp extern
-#[no_mangle]
-pub unsafe extern "C" fn memcmp(cx: *const u8, ct: *const u8, n: usize) -> i32 {
-	ext_memcmp(cx, ct, n)
-}
-
-/// memcpy extern
-#[no_mangle]
-pub unsafe extern "C" fn memcpy(dest: *mut u8, src: *const u8, n: usize) -> *mut u8 {
-	ext_memcpy(dest, src, n)
-}
-
-/// memmove extern
-#[no_mangle]
-pub unsafe extern "C" fn memmove(dest: *mut u8, src: *const u8, n: usize) -> *mut u8 {
-	ext_memmove(dest, src, n)
-}
-
-/// memset extern
-#[no_mangle]
-pub unsafe extern "C" fn memset(dest: *mut u8, c: i32, n: usize) -> *mut u8 {
-	ext_memset(dest, c, n)
-}
+extern crate rlibc;
