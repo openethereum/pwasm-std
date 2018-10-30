bc9c5a3e339f00f380e1d5323d7b08e05971238b
Herobird <robbepop@web.de>
2018-10-30 18:05:59 +0100

    Fix some indentation and formatting problems

--- src/types.rs
+++ src/types.rs
@@ -5,17 +5,17 @@
 pub use uint::U256;
 
 construct_fixed_hash!{
-    /// A 160 bits (20 bytes) hash type.
-    ///
-    /// # Note
-    ///
-    /// Can be interchanged with `Address`
-    pub struct H160(20);
+	/// A 160 bits (20 bytes) hash type.
+	///
+	/// # Note
+	///
+	/// Can be interchanged with `Address`
+	pub struct H160(20);
 }
 
 construct_fixed_hash!{
-    /// A 256-bits (32 bytes) hash type.
-    pub struct H256(32);
+	/// A 256-bits (32 bytes) hash type.
+	pub struct H256(32);
 }
 
 /// Represents an address in ethereum context.
@@ -27,16 +27,17 @@ construct_fixed_hash!{
 pub struct Address(H160);
 
 impl From<H160> for Address {
-    fn from(hash: H160) -> Self {
-        Address(hash)
-    }
+	fn from(hash: H160) -> Self {
+		Address(hash)
+	}
 }
 
 impl Address {
-    /// Returns the inner `H160` hash type of `self`.
-    pub fn into_inner(self) -> H160 {
-        self.0
-    }
+	/// Returns the inner `H160` hash type of `self`.
+	pub fn into_inner(self) -> H160 {
+		self.0
+	}
+}
 
 impl From<U256> for H256 {
 	fn from(uint: U256) -> H256 {
