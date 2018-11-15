f1236bce3c1b63bec3c33b9f6294510b1834ff15
David Yan <david.yan@berkeley.edu>
2018-11-15 00:48:09 +0000

    Revert crypto.rs to 0.10 version

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,5 +1,5 @@
 use tiny_keccak::Keccak;
-use types::H256;
+use hash::H256;
 
 /// Compute keccak hash.
 ///
@@ -8,24 +8,21 @@ use types::H256;
 /// ```rust
 /// use owasm_std::keccak;
 ///
-/// let hash = keccak("hello world".as_bytes());
+/// let hash = keccak("hello world");
 ///
-/// let expected = types::H256::from([
+/// let expected = &[
 /// 	71, 23, 50, 133, 168, 215, 52, 30,
 /// 	94, 151, 47, 198, 119, 40, 99, 132,
 /// 	248, 2, 248, 239, 66, 165, 236, 95,
 /// 	3, 187, 250, 37, 76, 176, 31, 173
-/// ]);
+/// ];
 ///
-/// assert_eq!(hash, expected);
+/// assert_eq!(hash.as_ref(), expected);
 /// ```
-pub fn keccak<T>(input: T) -> H256
-where
-	T: AsRef<[u8]>
-{
+pub fn keccak<T>(input: T) -> H256 where T: AsRef<[u8]> {
 	let mut keccak = Keccak::new_keccak256();
-	let mut res = H256::zero();
+	let mut res = H256::new();
 	keccak.update(input.as_ref());
-	keccak.finalize(res.as_bytes_mut());
+	keccak.finalize(&mut res);
 	res
 }
